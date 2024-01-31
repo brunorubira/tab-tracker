6fb836be2f5df7027afaa8789fb4a1001b0d15ef
brunorubira <brunorcorrea@hotmail.com>
2024-01-30 22:45:01 -0300

    server_on4

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -5,21 +5,26 @@
     <br>
     <input type="password" name="password" placeholder="password" v-model="password">
     <br>
-    <button>Register</button>
+    <button @click="register">Register</button>
   </div>
 </template>
 
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data() {
     return {
       email: '',
       password: ''
-
     }
-  }
+  },
+  watch: {
+    email(value) {
+      console.log('Mudou', value)
+    }
+  },
+  methods: { register() { console.log('Clicado Registrar', this.email, this.password) } }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped></style>
